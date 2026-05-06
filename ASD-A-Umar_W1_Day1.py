974802d25d88db3364618b45be90d6a3871689e7
Asma Umar <binteumar7595@gmail.com>
2026-05-06 06:29:49 -0700

    Add initial Python script for programming milestone
    This code includes my first programming milestone and basic input/variable practice from As-Sa’adah Institute.

---
+++ ASD-A-Umar_W1_Day1.py
@@ -0,0 +1,12 @@
+# ASSAADA’H TAMHI’D: WEEK 1 – DAY 1
+# Student: Asma Umar | Institute: As-Sa’adah Institute
+
+# My First Milestone
+print("My first step in programming")
+
+# Variables & Inputs Practice
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Welcome", name)
+print("Next year you will be", age + 1,"year old")
