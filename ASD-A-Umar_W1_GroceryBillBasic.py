b6e331829b7e9677b108160ba32db19cf6ed5b96
Asma Umar <binteumar7595@gmail.com>
2026-05-06 20:45:12 -0700

    Add basic grocery bill calculator
    : A simple program to sum up multiple item prices and display the total amount.

---
+++ ASD-A-Umar_W1_GroceryBillBasic.py
@@ -0,0 +1,13 @@
+# Task: Retail Transaction Summary (Grocery Bill)
+# Student: Asma Umar | Institute: As-Sa’adah Institute
+
+print("Grocery Bill")
+print("-" * 20)
+item1 = float(input("Enter price of item 1: "))
+item2 = float(input("Enter price of item 2: "))
+item3 = float(input("Enter price of item 3: "))
+
+total_bill = item1 + item2 + item3
+
+print("-" * 20)
+print(f"Total Amount Due: {total_bill}")
