0b3366443946c4655adb52df66112730bbef7b3d
Asma Umar <binteumar7595@gmail.com>
2026-05-06 20:24:59 -0700

    Add Celsius to Fahrenheit Converter
    A simple utility to convert temperature from Celsius to Fahrenheit. This project focuses on implementing mathematical formulas in Python and working with float data types for precise calculations.

---
+++ ASD-A-Umar_W1_TempConverter.py
@@ -0,0 +1,6 @@
+# Assignment 2: Temperature Converter (Celsius to Fahrenheit)
+# Student: Asma Umar | Institute: As-Sa’adah Institute
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"Temperature in Fahrenheit is: {fahrenheit}")
