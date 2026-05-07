844c3cbcac062bc193e4f13af1825e11e172d33f
Asma Umar <binteumar7595@gmail.com>
2026-05-06 20:26:35 -0700

    Add Even or Odd Number Checker
    This program introduces the concept of the modulo operator (%) to determine if a number is even or odd. It is a foundational exercise in conditional logic and remainder-based calculations.

---
+++ ASD-A-Umar_W1_EvenOdd.py
@@ -0,0 +1,8 @@
+# Assignment 3: Even/Odd Checker
+# Student: Asma Umar | Institute: As-Sa’adah Institute
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"The number {num} is Even.")
+else:
+    print(f"The number {num} is Odd.")
